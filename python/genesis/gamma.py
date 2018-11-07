fbdefb53b86212bbc00db5d72d11095ed4d21431
Daniel Topa <dantopa@lanl.gov>
2018-11-06 20:19:33 -0700

    new file:   python/genesis/short.rst

--- python/genesis/gamma.py
+++ python/genesis/gamma.py
@@ -1,5 +1,12 @@
 # https://stackoverflow.com/questions/8369219/how-do-i-read-a-text-file-into-a-string-variable-in-python
 from pathlib import Path
-txt = Path( "AmanziInputSpec-v2.3.2-draft.rst" ).read_text()
+# txt = Path( "AmanziInputSpec-v2.3.2-draft.rst" ).read_text()
+file_source="short.rst"
+txt = Path( file_source ).read_text()
 
 print ( "%s" % txt )
+
+numLines = txt.count( "\n" )
+
+print ( "reading file %s" % file_source )
+print ( "%s lines found" % numLines )

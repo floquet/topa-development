2cb3c170f8b1d2fb4526b692400ea7aced748fa2
Daniel Topa <dantopa@lanl.gov>
2018-11-20 20:42:15 -0700

    sys.version added

--- python/class/class-alpha.py
+++ python/class/class-alpha.py
@@ -14,7 +14,10 @@ class Vehicle:
     def number_of_wheels(self, number):
         self.number_of_wheels = number
 
-tesla_model_s = Vehicle(4, 'electric', 5, 250)
-print(tesla_model_s.number_of_wheels) # 4
-tesla_model_s.number_of_wheels = 2 # setting number of wheels to 2
+#tesla_model_s = Vehicle(4, 'electric', 5, 250)
+#print(tesla_model_s.number_of_wheels) # 4
+#tesla_model_s.number_of_wheels = 2 # setting number of wheels to 2
 # print(tesla_model_s.number_of_wheels) # 2
+
+tesla_model_s = Vehicle(4, 'electric', 5, 250)
+tesla_model_s.make_noise() # VRUUUUUUUM
---
+++ python/class/employee.py
@@ -0,0 +1,14 @@
+# https://www.youtube.com/watch?v=ZDa-Z5JzLYM
+import sys # python version
+
+class Employee:
+    pass
+
+emp_1 = Employee( )
+emp_2 = Employee( )
+
+print( emp_1 )
+print( emp_2 )
+
+print( "python version:" )
+print( sys.version )

9d1683fbb1ae8d6a0b1a7daba632b301f8b132cd
Daniel Topa <dantopa@lanl.gov>
2018-11-20 19:42:10 -0700

    setter, getter

---
+++ python/class/argon.py
@@ -0,0 +1,13 @@
+class Chapter:
+    def __init__(self, sections):
+        self.number_of_wheels = number_of_wheels
+        self.type_of_tank = type_of_tank
+        self.seating_capacity = seating_capacity
+        self.maximum_velocity =
+
+class Sections:
+    def __init__(self, sections):
+        self.number_of_wheels = number_of_wheels
+        self.type_of_tank = type_of_tank
+        self.seating_capacity = seating_capacity
+        self.maximum_velocity = maximum_velocity

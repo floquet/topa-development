f100ad2c411a6f0d4e576abe798f7c12d8f0b19c
Daniel Topa <dantopa@lanl.gov>
2018-11-21 10:25:51 -0700

    getters, setters, more advanced

---
+++ python/class/objects-python-A.py
@@ -0,0 +1,47 @@
+# https://stackoverflow.com/questions/2627002/whats-the-pythonic-way-to-use-getters-and-setters
+
+class C(object):
+    def __init__(self):
+        self._x = None
+
+    @property
+    def x( self ):
+        """I'm the 'x' property."""
+        print( "getter of x called: self._x = ", self._x )
+        return self._x
+
+    @x.setter
+    def x( self, value ):
+        print("setter of x called: self._x = ", value )
+        self._x = value
+
+    @x.deleter
+    def x( self ):
+        print( "deleter of x called" )
+        del self._x
+        print( "\nattempting to print self._x: disaster entails" )
+        print( "self._x = ", self._x )
+
+c = C()
+c.x = 'foo'  # setter called
+foo = c.x    # getter called
+del c.x      # deleter called
+
+# l127914@pn1249300.lanl.gov:class $ python objects-python-A.py
+# setter of x called: self._x =  foo
+# getter of x called: self._x =  foo
+# deleter of x called
+#
+# attempting to print self._x: disaster entails
+# Traceback (most recent call last):
+#   File "objects-python-A.py", line 28, in <module>
+#     del c.x      # deleter called
+#   File "objects-python-A.py", line 23, in x
+#     print( "self._x = ", self._x )
+# AttributeError: 'C' object has no attribute '_x'
+
+# l127914@pn1249300.lanl.gov:class $ date
+# Wed Nov 21 10:25:02 MST 2018
+
+# l127914@pn1249300.lanl.gov:class $ pwd
+# /Volumes/Tlaltecuhtli/repos/GitHub/topa-development/python/class
--- python/prototype/argon.py
+++ python/prototype/argon.py
@@ -11,27 +11,67 @@ import os           # probe, change directories
 import sys          # python version
 import datetime     # https://stackoverflow.com/questions/415511/how-to-get-the-current-time-in-python
 
+# # globals
+dingbat_block = "* * *"
+
+# # classes
+class Book:
+    """parsed, structured contents of #.rst file"""
+
+    def __init__( self, file_name, file_path, number_lines, number_sections ):
+        self.file_name       = file_name
+        self.file_path       = file_path
+        self.file_source     = file_path + file_name
+        self.number_lines    = number_lines
+        self.number_sections = number_sections
+    # setters
+    @number_lines.setter
+    def number_lines( self, count ):
+        self.number_lines = count
+    # getters
+
 # # program blocks
-def rst_file_harvest( ):
-    pass
+def rst_file_harvest( myFile ):
+    print( "\n", dingbat_block, "rst_file_harvest ", datetime.datetime.now( ) )
 
-def rst_file_process( ):
-    pass
+    file_path = "/Volumes/Tlaltecuhtli/repos/GitHub/topa-development/data/"
+    myBook = Book( myFile, file_path, -1, -1 )  # instantiate Book object
 
-def publish( ):
-    pass
+    ## ## read source file
+    print ( "reading file %s" % file_source )
+    # https://stackoverflow.com/questions/3277503/in-python-how-do-i-read-a-file-line-by-line-into-a-list
+    with open( file_source ) as f:
+        myLines = f.read().splitlines()  # remove \n
+
+    numLines = len( myLines )
+    print ( "%s lines found" % numLines )
+    return myLines;
+
+def rst_file_process( myFile ):
+    print( "\n", dingbat_block, "rst_file_process ", datetime.datetime.now( ) )
+
+    return;
+
+def publish_xlsx( ):
+    print( "\n", dingbat_block, "publish_xlsx ", datetime.datetime.now( ) )
+
+    return;
 
 def emulate_process( ):
-    pass
+    print( "\n", dingbat_block, "emulate_process ", datetime.datetime.now( ) )
+
+    return;
 
 def session_tag( ):
+    print( "\n", dingbat_block, "session_tag ", datetime.datetime.now( ) )
+
     # forensic data
     print( "\npython version")
     print( sys.version )
 
     print( "\nfile:     ", os.path.basename( __file__ ) )
     print(   "directory:", os.getcwd( ) )
-    
+
     print( datetime.datetime.now( ) )
 
 
@@ -39,8 +79,9 @@ def session_tag( ):
 
 if __name__ == "__main__":
 
+    file_name = "sample.txt"
 
-    rst_file_harvest( )
-    rst_file_process( )
-    publish( )
+    myLines = rst_file_harvest( file_name )
+    rst_file_process( myLines )
+    publish_xlsx( )
     session_tag( )

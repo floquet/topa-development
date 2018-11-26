ffdab40475e8d1adb47edde208ed3311dc0318c1
Daniel Topa <dantopa@lanl.gov>
2018-11-26 14:52:31 -0700

    worksheet gymnastics

--- python/xlsx/workseets/aluminum.py
+++ python/xlsx/workseets/aluminum.py
@@ -13,15 +13,24 @@
 # # https://xlsxwriter.readthedocs.io/page_setup.html
 
 # # imports
-import os           # probe, change directories
-import sys          # python version
-import datetime     # https://stackoverflow.com/questions/415511/how-to-get-the-current-time-in-python
+# import os           # probe, change directories
+# import sys          # python version
+# import datetime     # https://stackoverflow.com/questions/415511/how-to-get-the-current-time-in-python
 import xlsxwriter   # API for Excel
 
 workbook_title = "workseet-tester.xlsx"
 workbook_object = xlsxwriter.Workbook( workbook_title )
 
 worksheet_title = "my worksheet"
-worksheet_object = workbook.add_worksheet( worksheet_title )
+worksheet_object = workbook_object.add_worksheet( worksheet_title )
 
-workbook.close()
+header3 = '&LPage &P of &N' + '&CFilename: &F' + '&RSheetname: &A'
+footer3 = '&LCurrent date: &D' + '&RCurrent time: &T'
+
+myheader = "&A"
+myfooter = "&L&D" + "&C &P / &N" + "&RCurrent time: &T"
+worksheet_object.set_header( header3 )
+worksheet_object.set_footer( footer3 )
+
+
+workbook_object.close()

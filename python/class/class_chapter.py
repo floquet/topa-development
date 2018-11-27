ded2c8322db0175f75e8235fd60eb29f9af7f04f
Daniel Topa <dantopa@lanl.gov>
2018-11-26 22:40:59 -0700

    sections are flowing into chapters

--- python/class/class_chapter.py
+++ python/class/class_chapter.py
@@ -8,10 +8,13 @@ class Section:
         self.title = title  # 8
 
 class Chapter:
-    def __init__( self, chap_num, chap_title, chap_sections ):
-        self.chap_num   = chap_num  # 8
-        self.chap_title = chap_title   # Boundary conditions
+    def __init__( self, chap_num, chap_title, chap_initials, chap_sections ):
+        self.chap_num      = chap_num  # 8
+        self.chap_title    = chap_title   # Boundary conditions
+        self.chap_initials = chap_initials   # Boundary conditions
         self.chap_sections = chap_sections
+        # https://stackoverflow.com/questions/134934/display-number-with-leading-zeros
+        self.chap_key      = str( chap_num ).zfill( 2 ) + "-" + chap_initials
 
 # #    # #    # #    # #    # #    # #
 
@@ -22,27 +25,19 @@ if __name__ == "__main__":
     sec_bc_liquid_phase    = Section( 1, "liquid_phase" )
     sec_bc_solid_phase     = Section( 1, "solid_phase" )
     # create chapter
-    chap_bc = Chapter( 8, "Boundary Conditions",
+    chap_bc = Chapter( 8, "Boundary Conditions", "BC",
                 [ sec_bc_assigned_region, sec_bc_liquid_phase, sec_bc_solid_phase] )
 
-    print( "chap_bc.chap_num: ",   chap_bc.chap_num )
-    print( "chap_bc.chap_title: ",   chap_bc.chap_title )
+    print( "chap_bc.chap_num: ",        chap_bc.chap_num )
+    print( "chap_bc.chap_title: ",      chap_bc.chap_title )
+    print( "chap_bc.chap_key: ",        chap_bc.chap_key )
+    print( "chap_bc.chap_sections: ",   chap_bc.chap_sections )
 
     print( datetime.datetime.now( ) )
 
-# dantopa@Lax-Millgram:class $ py employee.py
-# python version:
-# 3.6.7 (default, Oct 21 2018, 09:26:25)
-# [GCC 4.2.1 Compatible Apple LLVM 9.1.0 (clang-902.0.39.2)]
-# <__main__.Employee object at 0x10aed7550>
-# <__main__.Employee object at 0x10aed75c0>
-# who am I?:  Daniel Topa
-
-# dantopa@Lax-Millgram:class $ date
-# Tue Nov 20 22:05:40 MST 2018
-
-# dantopa@Lax-Millgram:class $ pwd
-# /Users/dantopa/Documents/GitHub/topa-development/python/class
-
-# dantopa@Lax-Millgram:class $ python --version
-# Python 3.6.7
+# dantopa@Lax-Millgram:class $ python class_chapter.py
+# chap_bc.chap_num:  8
+# chap_bc.chap_title:  Boundary Conditions
+# chap_bc.chap_key:  08-BC
+# chap_bc.chap_sections:  [<__main__.Section object at 0x109a542b0>, <__main__.Section object at 0x10962d5c0>, <__main__.Section object at 0x10962d5f8>]
+# 2018-11-26 22:39:39.771564

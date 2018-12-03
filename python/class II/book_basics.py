4a341de823179fa7628af087258e844c77d74e92
Daniel Topa <dantopa@lanl.gov>
2018-12-03 12:17:19 -0700

    book_basics.py finished

--- python/class II/book_basics.py
+++ python/class II/book_basics.py
@@ -1,5 +1,11 @@
 # https://stackoverflow.com/questions/2627002/whats-the-pythonic-way-to-use-getters-and-setters
 
+# # imports
+import datetime         # timestamps
+import os               # probe, change directories
+
+#  ==   ==   == ==   ==   == ==   ==   == ==   ==   ==  #
+
 class Book(object):
     def __init__(self):
         self._file_name = None
@@ -35,6 +41,8 @@ if __name__ == "__main__":
     print( "myBook.file_name = ", myBook.file_name )
     print( "foo = ", foo )
     del myBook.file_name             # deleter called
+    print( datetime.datetime.now( ) )
+    print( "source: %s/%s" % ( os.getcwd( ), os.path.basename( __file__ ) ) )
     print( "myBook.file_name = ", myBook.file_name )
 
 # l127914@pn1249300.lanl.gov:class II $ python book_basics.py
@@ -44,15 +52,11 @@ if __name__ == "__main__":
 # myBook.file_name =  file.rst
 # foo =  file.rst
 # deleter of x called
+# 2018-12-03 12:16:34.725406
+# source: /Volumes/Tlaltecuhtli/repos/GitHub/topa-development/python/class II/book_basics.py
 # Traceback (most recent call last):
-#   File "book_basics.py", line 38, in <module>
+#   File "book_basics.py", line 46, in <module>
 #     print( "myBook.file_name = ", myBook.file_name )
-#   File "book_basics.py", line 11, in file_name
+#   File "book_basics.py", line 17, in file_name
 #     print( "getter of file_name called: self._file_name = ", self._file_name )
 # AttributeError: 'Book' object has no attribute '_file_name'
-
-# l127914@pn1249300.lanl.gov:class II $ pwd
-# /Volumes/Tlaltecuhtli/repos/GitHub/topa-development/python/class II
-
-# l127914@pn1249300.lanl.gov:class II $ date
-# Mon Dec  3 12:09:29 MST 2018

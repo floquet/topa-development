5542f6937163e41088c2cdc95975e9cf38571ade
Daniel Topa <dantopa@lanl.gov>
2018-12-04 21:55:01 -0700

    bavius.py writes dramatis_personae

--- amanzi/bourbaki/tools_debug.py
+++ amanzi/bourbaki/tools_debug.py
@@ -12,7 +12,25 @@ import tools_xl             # spreadsheet authoring tools
 #  ==   ==   == ==   ==   == ==   ==   == ==   ==   ==  #
 
 def xl_dramatis_personae( thisWorkbook, thisBook ):
+
     sheet_dramatis_personae = tools_xl.xl_sheet_generate( thisWorkbook, "dramatis personae" )
+
+    # restablish (?) formats
+    format_title = thisWorkbook.add_format( )
+    format_title.set_bold( )
+    format_title.set_font_color( "blue" )
+
+    # address worksheet in row, col form
+    row = 0;
+    col = 0;
+    sheet_dramatis_personae.write( row, col, "Source file", format_title ); row += 1
+
+    sheet_dramatis_personae.write( row, col, "File name" )
+    sheet_dramatis_personae.write( row, col + 1, thisBook.source_object.input_rst ); row += 1
+
+    sheet_dramatis_personae.write( row, col, "Path" )
+    sheet_dramatis_personae.write( row, col + 1, thisBook.source_object.path_rst ); row += 1
+
 
     return;
 

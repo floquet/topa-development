679d13cd17f701d74b390e16f43e927f1f37c38f
Daniel Topa <dantopa@lanl.gov>
2018-12-04 22:07:45 -0700

    going to bed

--- amanzi/bourbaki/tools_debug.py
+++ amanzi/bourbaki/tools_debug.py
@@ -14,6 +14,8 @@ import tools_xl             # spreadsheet authoring tools
 def xl_dramatis_personae( thisWorkbook, thisBook ):
 
     sheet_dramatis_personae = tools_xl.xl_sheet_generate( thisWorkbook, "dramatis personae" )
+    # widen first column
+    sheet_dramatis_personae.set_column( "A:A", 10 )
 
     # restablish (?) formats
     format_title = thisWorkbook.add_format( )
@@ -23,17 +25,31 @@ def xl_dramatis_personae( thisWorkbook, thisBook ):
     # address worksheet in row, col form
     row = 0;
     col = 0;
+
+    # source
     sheet_dramatis_personae.write( row, col, "Source file", format_title ); row += 1
 
     sheet_dramatis_personae.write( row, col, "Title" )
     sheet_dramatis_personae.write( row, col + 1, thisBook.source_object.title ); row += 1
 
-    sheet_dramatis_personae.write( row, col, "File name" )
+    sheet_dramatis_personae.write( row, col, "Data file" )
     sheet_dramatis_personae.write( row, col + 1, thisBook.source_object.input_rst ); row += 1
 
-    sheet_dramatis_personae.write( row, col, "Path" )
+    sheet_dramatis_personae.write( row, col, "Data path" )
     sheet_dramatis_personae.write( row, col + 1, thisBook.source_object.path_rst ); row += 1
 
+    sheet_dramatis_personae.write( row, col, "Lines read" )
+    sheet_dramatis_personae.write( row, col + 1, thisBook.source_object.numLines ); row += 1
+
+    # sink
+    row += 1
+    sheet_dramatis_personae.write( row, col, "Output file", format_title ); row += 1
+
+    sheet_dramatis_personae.write( row, col, "Output file" )
+    sheet_dramatis_personae.write( row, col + 1, thisBook.source_object.output_xl ); row += 1
+
+    sheet_dramatis_personae.write( row, col, "Output path" )
+    sheet_dramatis_personae.write( row, col + 1, thisBook.source_object.path_xl ); row += 1
 
     return;
 

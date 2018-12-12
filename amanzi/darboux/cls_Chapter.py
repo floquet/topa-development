81513faf5fad5086e12d996d5325265511fb9902
Daniel Topa <dantopa@lanl.gov>
2018-12-12 16:45:50 -0700

    new cls_Chapter.py

--- amanzi/darboux/cls_Chapter.py
+++ amanzi/darboux/cls_Chapter.py
@@ -3,28 +3,32 @@
 # # Amanzi: The Multi-Process HPC Simulator
 # #   https://github.com/amanzi/amanzi
 # # David Moulton DGL LANL/T-5 moulton@lanl.gov 505 665 4712
-# # Daniel Topa LANL/CCS-2 dantopa@lanl.gov 505 667 0817
+# # Daniel Topa   LANL/CCS-2   dantopa@lanl.gov 505 667 0817
 
-import        cls_Chapter # chapter (constains sections)
-
-# Book class
+# Chapter class
 #  commentary
 
-class Book( object ):
+class Chapter( object ):
     def __init__( self ):
 
-        self._title           = None    # Amanzi XML Input Specification (Version 2.3-draft)
+        self._title           = None    # Model Description
+        self._key             = None    # 08-MD
         self._k_start         = None    # 5
         self._k_stop          = None    # 231
-        self._col_chapter     = list( ) # collection of chapters
+        self._col_sections    = list( ) # collection of sections
 
 
 #   P R O P E R T I E S   #
 
     @property
     def title( self ):
-        """Title of book"""
+        """Chapter title"""
         return self._title
+
+    @property
+    def key( self ):
+        """Unique key to tag chapter"""
+        return self._key
 
     @property
     def k_start( self ):
@@ -37,15 +41,19 @@ class Book( object ):
         return self._k_stop
 
     @property
-    def col_chapter( self ):
-        """Collection of chapter objects"""
-        return self._col_chapter
+    def col_sections( self ):
+        """Collection of section objects"""
+        return self._col_sections
 
 #   S E T T E R S   #
 
     @title.setter
     def title( self, value ):
         self._title = value
+
+    @key.setter
+    def key( self, value ):
+        self._key = value
 
     @k_start.setter
     def k_start( self, value ):
@@ -55,15 +63,19 @@ class Book( object ):
     def k_stop( self, value ):
         self._k_stop = value
 
-    @col_chapter.setter
-    def col_chapter( self, value ):
-        self._col_chapter = value
+    @col_sections.setter
+    def col_sections( self, value ):
+        self._col_sections = value
 
 #   D E L E T E R S   #
 
     @title.deleter
     def title( self ):
         del self._title
+
+    @key.deleter
+    def key( self ):
+        del self._key
 
     @k_start.deleter
     def k_start( self ):
@@ -73,20 +85,18 @@ class Book( object ):
     def k_stop( self ):
         del self._k_stop
 
-    @col_chapter.deleter
-    def col_chapter( self ):
-        del self._col_chapter
+    @col_sections.deleter
+    def col_sections( self ):
+        del self._col_sections
 
 #  ==   ==   == ==   ==   == ==   ==   == ==   ==   ==  #
 
-# user: dantopa, CPU: Lax-Millgram, MM v. 11.3.0 for Mac OS X x86,
-# date: Dec 12, 2018, time: 16:37:20,
-# nb: /Users/dantopa/Mathematica_files/nb/lanl/python/author/class-structures-03.nb
+# user: dantopa, CPU: Lax-Millgram, MM v. 11.3.0 for Mac OS X x86, date: Dec 12, 2018, time: 16:40:24, nb: /Users/dantopa/Mathematica_files/nb/lanl/python/author/class-structures-03.nb
 
-# dantopa@Lax-Millgram:darboux $ py cls_Book.py 
+# dantopa@Lax-Millgram:darboux $ py cls_Chapter.py
 
 # dantopa@Lax-Millgram:darboux $ date
-# Wed Dec 12 16:42:40 MST 2018
+# Wed Dec 12 16:43:28 MST 2018
 
 # dantopa@Lax-Millgram:darboux $ pwd
 # /Users/dantopa/Documents/repos/GitHub/topa-development/amanzi/darboux

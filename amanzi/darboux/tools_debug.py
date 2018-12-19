847a9c822873c3cb1a97943bfd2c53a1b8445137
Daniel Topa <dantopa@lanl.gov>
2018-12-19 12:22:48 -0700

    shorten sheet names

--- amanzi/darboux/tools_debug.py
+++ amanzi/darboux/tools_debug.py
@@ -5,8 +5,8 @@
 # # Daniel Topa     LANL/CCS-2    dantopa@lanl.gov  505 667 0817
 
 # # Debug tools
-# xl_numbered_lines( thisWorkbook, theseLines )
-# xl_dramatis_personae( thisWorkbook, thisBook )
+#   xl_numbered_lines( thisWorkbook, theseLines )
+#   xl_dramatis_personae( thisWorkbook, thisBook )
 
 # # imports
 import tools_xl             # spreadsheet authoring tools
@@ -14,7 +14,6 @@ import tools_xl             # spreadsheet authoring tools
 #  ==   ==   == ==   ==   == ==   ==   == ==   ==   ==  #
 
 def xl_chapter_attributes( thisWorkbook, col_chapters ):
-    #print( "=  =  = xl_chapter_attributes: %s" % len( col_chapters ) )
     # create sheet
     s = tools_xl.xl_sheet_generate( thisWorkbook, "chapters" )
 
@@ -30,8 +29,8 @@ def xl_chapter_attributes( thisWorkbook, col_chapters ):
         s.write( row, col + 2, c.k_start )
         s.write( row, col + 3, c.k_stop ); row += 1
 
-        s.write( row, col, "key:" )
-        s.write( row, col + 1, c.key ); row += 2
+        s.write( row, col + 1, "key:" )
+        s.write( row, col + 2, c.key ); row += 2
 
     return
 
@@ -39,7 +38,7 @@ def xl_chapter_attributes( thisWorkbook, col_chapters ):
 
 def xl_numbered_lines( thisWorkbook, theseLines ):
     print( "=  =  = xl_numbered_lines:" )
-    sheet_numbered_lines = tools_xl.xl_sheet_generate( thisWorkbook, "text lines" )
+    s = tools_xl.xl_sheet_generate( thisWorkbook, "text lines" )
 
     # address worksheet in row, col form
     row = 0;
@@ -54,35 +53,35 @@ def xl_numbered_lines( thisWorkbook, theseLines ):
     format_title.set_bold( )
     format_title.set_font_color( "blue" )
 
-    sheet_numbered_lines.write( row, col,     "index",  format_title )
-    sheet_numbered_lines.write( row, col + 1, "length", format_title )
-    sheet_numbered_lines.write( row, col + 2, "text",   format_title ); row += 1
+    s.write( row, col,     "index",  format_title )
+    s.write( row, col + 1, "length", format_title )
+    s.write( row, col + 2, "text",   format_title ); row += 1
 
     for line in theseLines:
-        sheet_numbered_lines.write( row, col, counter )
+        s.write( row, col, counter )
         myString = '= TEXT( "' + line + '", "=" )'
-        #sheet_numbered_lines.write_formula( row, col + 1, myString )
-        sheet_numbered_lines.write( row, col + 1, len( line ) )
-        sheet_numbered_lines.write( row, col + 2, "boo:", myText )
-        sheet_numbered_lines.write( row, col + 2, line, myText ); row += 1
-        #sheet_numbered_lines.write( row, col + 2, myString, myText ); row += 1
+        #s.write_formula( row, col + 1, myString )
+        s.write( row, col + 1, len( line ) )
+        s.write( row, col + 2, "boo:", myText )
+        s.write( row, col + 2, line, myText ); row += 1
+        #s.write( row, col + 2, myString, myText ); row += 1
         counter += 1;
     # https://stackabuse.com/read-a-file-line-by-line-in-python/
     # with open( thisBook.source.full_rst ) as f:
     #     for cnt, line in enumerate( f ):
     #         # line number
-    #         sheet_numbered_lines.write( row, col,     cnt )
+    #         s.write( row, col,     cnt )
     #         # text
-    #         sheet_numbered_lines.write( row, col + 1, "{}".format( line ) ); row += 1
+    #         s.write( row, col + 1, "{}".format( line ) ); row += 1
     # https://stackabuse.com/read-a-file-line-by-line-in-python/
     # with open( thisBook.source.full_rst ) as f:
     #     for cnt, line in enumerate( f ):
     #         # line number
-    #         sheet_numbered_lines.write( row, col, cnt )
+    #         s.write( row, col, cnt )
     #         # text
     #         myString = '= TEXT( "' + line + '", "=" )'
     #         print( "myString = %s" % myString )
-    #         sheet_numbered_lines.write_formula( row, col + 1, myString ); row += 1
+    #         s.write_formula( row, col + 1, myString ); row += 1
     return
 
 #  ==   ==   == ==   ==   == ==   ==   == ==   ==   ==  #
@@ -90,9 +89,9 @@ def xl_numbered_lines( thisWorkbook, theseLines ):
 def xl_dramatis_personae( thisWorkbook, thisBook ):
     # print( "=  =  = xl_dramatis_personae:" )
 
-    sheet_dramatis_personae = tools_xl.xl_sheet_generate( thisWorkbook, "dramatis personae" )
+    s = tools_xl.xl_sheet_generate( thisWorkbook, "dramatis personae" )
     # widen first column
-    sheet_dramatis_personae.set_column( "A:A", 10 )
+    s.set_column( "A:A", 10 )
 
     # restablish (?) formats
     format_title = thisWorkbook.add_format( )
@@ -104,29 +103,29 @@ def xl_dramatis_personae( thisWorkbook, thisBook ):
     col = 0;
 
     # source
-    sheet_dramatis_personae.write( row, col, "Source file", format_title ); row += 1
+    s.write( row, col, "Source file", format_title ); row += 1
 
-    sheet_dramatis_personae.write( row, col, "Title" )
-    sheet_dramatis_personae.write( row, col + 1, thisBook.source.title ); row += 1
+    s.write( row, col, "Title" )
+    s.write( row, col + 1, thisBook.source.title ); row += 1
 
-    sheet_dramatis_personae.write( row, col, "Data file" )
-    sheet_dramatis_personae.write( row, col + 1, thisBook.source.input_rst ); row += 1
+    s.write( row, col, "Data file" )
+    s.write( row, col + 1, thisBook.source.input_rst ); row += 1
 
-    sheet_dramatis_personae.write( row, col, "Data path" )
-    sheet_dramatis_personae.write( row, col + 1, thisBook.source.path_rst ); row += 1
+    s.write( row, col, "Data path" )
+    s.write( row, col + 1, thisBook.source.path_rst ); row += 1
 
-    sheet_dramatis_personae.write( row, col, "Lines read" )
-    sheet_dramatis_personae.write( row, col + 1, thisBook.source.numLines ); row += 1
+    s.write( row, col, "Lines read" )
+    s.write( row, col + 1, thisBook.source.numLines ); row += 1
 
     # sink
     row += 1
-    sheet_dramatis_personae.write( row, col, "Output file", format_title ); row += 1
+    s.write( row, col, "Output file", format_title ); row += 1
 
-    sheet_dramatis_personae.write( row, col, "Output file" )
-    sheet_dramatis_personae.write( row, col + 1, thisBook.source.output_xl ); row += 1
+    s.write( row, col, "Output file" )
+    s.write( row, col + 1, thisBook.source.output_xl ); row += 1
 
-    sheet_dramatis_personae.write( row, col, "Output path" )
-    sheet_dramatis_personae.write( row, col + 1, thisBook.source.path_xl ); row += 1
+    s.write( row, col, "Output path" )
+    s.write( row, col + 1, thisBook.source.path_xl ); row += 1
 
     return;
 
--- amanzi/darboux/tools_xl.py
+++ amanzi/darboux/tools_xl.py
@@ -8,7 +8,7 @@
 # xl_new_workbook( workbook_title )
 # xl_sheet_requirements( this_workbook )
 # xl_sheet_generate( this_workbook, title_sheet )
-# xl_sheet_provenance( this_workbook )
+# xl_s( this_workbook )
 # xl_sheet_header_footer( this_worksheet )
 
 # # imports
@@ -22,7 +22,7 @@ def xl_new_workbook( workbook_title ):
 
     my_workbook = xlsxwriter.Workbook( workbook_title )
 
-    xl_sheet_provenance( my_workbook ) # provenance sheet
+    xl_s( my_workbook ) # provenance sheet
     xl_sheet_requirements( my_workbook ) # summarize requirements by type
 
     return my_workbook;
@@ -50,9 +50,9 @@ def xl_sheet_requirements( this_workbook ):
     sheet_requirements_fail.write( row, col, "Summary or requirements FAIL", format_title )
 
     # NULL
-    sheet_requirements_null = this_workbook.add_worksheet( "requirements - NULL" )
-    xl_sheet_header_footer( sheet_requirements_null )
-    sheet_requirements_null.write( row, col, "Summary or requirements NULL", format_title )
+    s = this_workbook.add_worksheet( "requirements - NULL" )
+    xl_sheet_header_footer( s )
+    s.write( row, col, "Summary or requirements NULL", format_title )
 
     return;
 
@@ -68,10 +68,10 @@ def xl_sheet_generate( this_workbook, title_sheet ):
 
 #  ==   ==   == ==   ==   == ==   ==   == ==   ==   ==  #
 
-def xl_sheet_provenance( this_workbook ):
+def xl_s( this_workbook ):
 
     # forensic info
-    sheet_provenance = xl_sheet_generate( this_workbook, "provenance" )
+    s = xl_sheet_generate( this_workbook, "provenance" )
 
     #  # special formats
     # https://xlsxwriter.readthedocs.io/format.html?highlight=bold
@@ -87,72 +87,72 @@ def xl_sheet_provenance( this_workbook ):
     format_time = this_workbook.add_format( {'num_format': 'yy/mm/dd hh:mm'} ) # https://xlsxwriter.readthedocs.io/working_with_dates_and_time.html
 
     # widen first columns
-    sheet_provenance.set_column( "A:A", 15 )
-    sheet_provenance.set_column( "B:B", 13 )
+    s.set_column( "A:A", 15 )
+    s.set_column( "B:B", 13 )
 
     # https://xlsxwriter.readthedocs.io/worksheet.html
-    sheet_provenance.write_url( "A1", "https://github.com/amanzi/amanzi", string = "AMANZI: The Multi-Process HPC Simulator" )
+    s.write_url( "A1", "https://github.com/amanzi/amanzi", string = "AMANZI: The Multi-Process HPC Simulator" )
 
     #  #  provencance
-    sheet_provenance.write( "A3", "Workbook created by", format_title )
-    #sheet_provenance.write( "A1", tip, "boo" )
+    s.write( "A3", "Workbook created by", format_title )
+    #s.write( "A1", tip, "boo" )
 
     # python notebook which creates workbook
-    sheet_provenance.write( "A4", "python source" )
-    sheet_provenance.write( "B4", os.path.basename( __file__ ) ) # charlie.py
+    s.write( "A4", "python source" )
+    s.write( "B4", os.path.basename( __file__ ) ) # charlie.py
 
     # current working directory
-    sheet_provenance.write( "A5", "directory" )
-    sheet_provenance.write( "B5", os.getcwd( ) ) # /Volumes/Tlaltecuhtli/repos/GitHub/topa-development/python/xlsx
+    s.write( "A5", "directory" )
+    s.write( "B5", os.getcwd( ) ) # /Volumes/Tlaltecuhtli/repos/GitHub/topa-development/python/xlsx
 
     # python version
-    sheet_provenance.write( "A6", "python version" )
-    sheet_provenance.write( "B6", sys.version ) # "3.7.0 (default, Jun 28 2018, 07:39:16) [Clang 4.0.1 (tags/RELEASE_401/final)]"
+    s.write( "A6", "python version" )
+    s.write( "B6", sys.version ) # "3.7.0 (default, Jun 28 2018, 07:39:16) [Clang 4.0.1 (tags/RELEASE_401/final)]"
 
     #  #  environment variables
     # practise row, col notation
     col = 0 # starting column
     row = 7 # starting row
-    sheet_provenance.write( row, col, "Environment variables", format_title ); row += 1
+    s.write( row, col, "Environment variables", format_title ); row += 1
 
-    sheet_provenance.write( row, col, "$USER" ) # l127914
-    sheet_provenance.write( row, col + 1, os.environ[ "USER" ] ); row += 1
+    s.write( row, col, "$USER" ) # l127914
+    s.write( row, col + 1, os.environ[ "USER" ] ); row += 1
 
-    sheet_provenance.write( row, col, "$HOSTNAME" ) # Cauchy.Schwarz
-    sheet_provenance.write( row, col + 1, os.environ[ "HOSTNAME" ] ); row += 1
+    s.write( row, col, "$HOSTNAME" ) # Cauchy.Schwarz
+    s.write( row, col + 1, os.environ[ "HOSTNAME" ] ); row += 1
 
-    sheet_provenance.write( row, col, "$HOME" ) # /Users/l127914
-    sheet_provenance.write( row, col + 1, os.environ[ "HOME" ] ); row += 1
+    s.write( row, col, "$HOME" ) # /Users/l127914
+    s.write( row, col + 1, os.environ[ "HOME" ] ); row += 1
 
-    sheet_provenance.write( row, col, "timestamp" ) # 11/21/18 16:18
-    sheet_provenance.write( row, col + 1, datetime.datetime.now( ), format_time ); row += 1
+    s.write( row, col, "timestamp" ) # 11/21/18 16:18
+    s.write( row, col + 1, datetime.datetime.now( ), format_time ); row += 1
 
     #  #  Excel info routines
     # https://xlsxwriter.readthedocs.io/working_with_formulas.html
 
     row += 1 # jump
-    sheet_provenance.write( row, col, "XL info function", format_title ); row += 1
+    s.write( row, col, "XL info function", format_title ); row += 1
 
-    sheet_provenance.write( row, col, "platform" ) # mac
-    sheet_provenance.write_formula( row, col + 1, '= INFO( "system" )' ); row += 1
+    s.write( row, col, "platform" ) # mac
+    s.write_formula( row, col + 1, '= INFO( "system" )' ); row += 1
 
-    sheet_provenance.write( row, col, "recalculation mode" ) # Automatic
-    sheet_provenance.write_formula( row, col + 1, '= INFO( "recalc" )' ); row += 1
+    s.write( row, col, "recalculation mode" ) # Automatic
+    s.write_formula( row, col + 1, '= INFO( "recalc" )' ); row += 1
 
-    sheet_provenance.write( row, col, "active sheets" ) # 1
-    sheet_provenance.write_formula( row, col + 1, '= INFO( "numfile" )' ); row += 1
+    s.write( row, col, "active sheets" ) # 1
+    s.write_formula( row, col + 1, '= INFO( "numfile" )' ); row += 1
 
-    sheet_provenance.write( row, col, "cursor" ) # $A:$A$1
-    sheet_provenance.write_formula( row, col + 1, '= INFO( "origin" )' ); row += 1
+    s.write( row, col, "cursor" ) # $A:$A$1
+    s.write_formula( row, col + 1, '= INFO( "origin" )' ); row += 1
 
-    sheet_provenance.write( row, col, "XL release" ) # 16.16
-    sheet_provenance.write_formula( row, col + 1, '= INFO( "release" )' ); row += 1
+    s.write( row, col, "XL release" ) # 16.16
+    s.write_formula( row, col + 1, '= INFO( "release" )' ); row += 1
 
-    sheet_provenance.write( row, col, "application directory" ) # /Users/dantopa/Library/Containers/com.microsoft.Excel/Data/Documents/
-    sheet_provenance.write_formula( row, col + 1, '= INFO( "directory" )' ); row += 1
+    s.write( row, col, "application directory" ) # /Users/dantopa/Library/Containers/com.microsoft.Excel/Data/Documents/
+    s.write_formula( row, col + 1, '= INFO( "directory" )' ); row += 1
 
-    sheet_provenance.write( row, col, "operating systems" ) # Macintosh (Intel) Version 10.13.3 (Build 17D47)
-    sheet_provenance.write_formula( row, col + 1, '= INFO( "osversion" )' ); row += 1
+    s.write( row, col, "operating systems" ) # Macintosh (Intel) Version 10.13.3 (Build 17D47)
+    s.write_formula( row, col + 1, '= INFO( "osversion" )' ); row += 1
 
     return;
 
